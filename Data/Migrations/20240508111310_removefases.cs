7a76a9acd0f73178d270ec2c46d96cc64028f69f
PMFA17 <pedromaral08.pa@gmail.com>
2024-05-14 14:35:44 +0100

    migrations

---
+++ Data/Migrations/20240508111310_removefases.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _8BallShot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class removefases : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fase1",
+                table: "Torneio");
+
+            migrationBuilder.DropColumn(
+                name: "Fase2",
+                table: "Torneio");
+
+            migrationBuilder.DropColumn(
+                name: "Fase3",
+                table: "Torneio");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Fase1",
+                table: "Torneio",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fase2",
+                table: "Torneio",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fase3",
+                table: "Torneio",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

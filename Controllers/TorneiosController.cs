d6b60baa5feec473d76aa0cd1bcaa93c78171116
PMFA17 <pedromaral08.pa@gmail.com>
2024-05-17 23:15:28 +0100

    bracket torneio na pagina privacy

--- Controllers/TorneiosController.cs
+++ Controllers/TorneiosController.cs
@@ -13,19 +13,47 @@ namespace _8BallShot.Controllers
     public class TorneiosController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly List<Jogador> _jogador;
 
         public TorneiosController(ApplicationDbContext context)
         {
             _context = context;
+
+            _jogador = new List<Jogador>
+            {
+                new Jogador { IdJogador = 1, NomeJogador = "Jogador 1", NIF = 123456789 },
+                new Jogador { IdJogador = 2, NomeJogador = "Jogador 2", NIF = 987654321 },
+                new Jogador { IdJogador = 3, NomeJogador = "Jogador 3", NIF = 123456329 },
+                new Jogador { IdJogador = 4, NomeJogador = "Jogador 4", NIF = 987633421 },
+            };
         }
 
         // GET: Torneios
         public async Task<IActionResult> Index()
         {
-              return _context.Torneio != null ? 
-                          View(await _context.Torneio.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Torneio'  is null.");
+            return View(_jogador);
+              //return _context.Torneio != null ? 
+              //            View(await _context.Torneio.ToListAsync()) :
+              //            Problem("Entity set 'ApplicationDbContext.Torneio'  is null.");
         }
+
+        [HttpPost]
+        public IActionResult Bracket(int player1, int player2)
+        {
+            var player1Selected = _jogador.Find(p => p.IdJogador == player1);
+            var player2Selected = _jogador.Find(p => p.IdJogador == player2);
+
+            if (player1Selected != null && player2Selected != null)
+            {
+                var model = new Tuple<Jogador, Jogador>(player1Selected, player2Selected);
+                return View("BracketView", model);
+            }
+            else
+            {
+                return NotFound(); // Trata o caso em que um ou ambos os jogadores não são encontrados
+            }
+        }
+
 
         // GET: Torneios/Details/5
         public async Task<IActionResult> Details(int? id)
